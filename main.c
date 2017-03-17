98a9444f550e9877036ea0c94521e5d26488245c
Clancy Emanuel <clancyemanuel@gmail.com>
2017-03-17 20:33:00 +0000

    expects a char *, not an int

--- main.c
+++ main.c
@@ -11,11 +11,29 @@
 #include <arpa/inet.h>
 
 static const int MAX_DATA_SIZE = 1024;
+static const char *MECA_ARM_IP_ADDR = "192.168.0.100";
+static const char *MECA_ARM_PORT = "10000";
 
 int main(int argc, char *argv[])
 {	
-	int sockfd, numbytes;
+	int sockfd, numbytes, rv;
 	char buf[MAX_DATA_SIZE];
-	struct sockaddr_in serverAddr;
+	struct addrinfo hints, *servinfo, *p;
+	char s[INET6_ADDRSTRLEN];
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;	
+
+	if((rv = getaddrinfo(MECA_ARM_IP_ADDR,
+		MECA_ARM_PORT,
+		&hints,
+		&servinfo)) !=0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	} 
+	
 	int armFd = socket(AF_INET, SOCK_STREAM, 0);
+	
+
 }
