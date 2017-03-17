fc66eab6d313e0f6f2baf3cf0cd2fee50489b4a3
Clancy Emanuel <clancyemanuel@gmail.com>
2017-03-17 20:40:28 +0000

    connected to robot

--- main.c
+++ main.c
@@ -14,6 +14,14 @@ static const int MAX_DATA_SIZE = 1024;
 static const char *MECA_ARM_IP_ADDR = "192.168.0.100";
 static const char *MECA_ARM_PORT = "10000";
 
+void *get_in_addr(struct sockaddr *sa)
+{
+	if(sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in *)sa)->sin_addr);
+	}
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
 int main(int argc, char *argv[])
 {	
 	int sockfd, numbytes, rv;
@@ -32,8 +40,45 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	} 
+
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if((sockfd = socket(p->ai_family, p->ai_socktype,
+		p->ai_protocol)) == -1) {
+			perror("client: connect");
+			continue;
+		}
+			
+		if(connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("client: connect");
+			continue;
+		}
+		
+		break;
+	}
 	
-	int armFd = socket(AF_INET, SOCK_STREAM, 0);
-	
+	if(p == NULL) {
+		fprintf(stderr, "client: failed to connect\n");
+		return 2;
+	}
+
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr*)p->ai_addr),
+		s, sizeof s);
+	printf("client: connecting to %s\n", s);
+
+	freeaddrinfo(servinfo);
+
+	if((numbytes = recv(sockfd, buf, MAX_DATA_SIZE-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}		
+
+	buf[numbytes] = '\0';
+
+	printf("client: received '%s'\n",buf);
+
+	close(sockfd);
+
+	return 0;
 
 }
