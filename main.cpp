f742e8a21d1c868c92b39d3ff6efaedc0e3c7df0
Clancy Emanuel <clancyemanuel@gmail.com>
2017-04-13 01:06:46 +0000

    Main converted

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "GetSocket.h"
+//#include "ActivateRobot.h"
+//#include "DestroySocket.h"
+//#include "HomeRobot.h"
+//#include "DeactivateRobot.h"
+//#include "SetJoints.h"
+
+int main()
+{
+	static const char *MECA_IP="192.168.0.100";
+	static const char *MECA_PORT="10000";
+	int robotSocket = GetSocket(MECA_IP, MECA_PORT);
+	//ActivateRobot(robotSocket);
+	//HomeRobot(robotSocket);
+	//SetJoints(robotSocket,0,0,0,0,0,0);
+	//DeactivateRobot(robotSocket);
+	//int destroy = DestroySocket(robotSocket);
+	return 0;
+}
