4f336236b3c3749098cb340b8254c96da9f199f5
Clancy Emanuel <clancyemanuel@gmail.com>
2017-04-13 00:07:42 +0000

    Got it working to activate robot without error. Error was caused by strlen for some reason

--- ActivateRobot.c
+++ ActivateRobot.c
@@ -6,10 +6,8 @@
 
 int ActivateRobot(int robotSocket)
 {
-	char activateRobotCommand[14] = "ActivateRobot ";
-	int msgLength = strlen(activateRobotCommand);
-	activateRobotCommand[13] = 0;
-	int ret = send(robotSocket, activateRobotCommand, msgLength, 0);
+	char activateRobotCommand[14] = "ActivateRobot";
+	int ret = send(robotSocket, activateRobotCommand, 14, 0);
 	ReadFromSocket(robotSocket);
 	return ret;
 }
