2526acec5aad750e6ba6fd969bd0d6538425cbb0
Clancy Emanuel <clancyemanuel@gmail.com>
2017-05-09 23:12:13 -0400

    Refactored a bit

--- java/meca500.java
+++ java/meca500.java
@@ -3,14 +3,35 @@ import java.net.*;
 import java.util.Arrays;
 
 class meca500{
+static DataOutputStream out = null;
+static DataInputStream in = null; 
+
+    
+    public static void SendMessage(String message){
+	message += "<\0>";
+	try{
+	    out.writeBytes("ActivateRobot<\0>");
+	}catch(IOException e){
+	    System.err.println("Couldn't write to robot");
+	}
+    }
+
+    public static void ReadMessage(){
+	byte[] response = new byte[100];
+        try{
+	    in.read(response);
+	    String responseString = new String(response,"UTF-8");
+	    System.out.println(responseString);
+	
+	}catch(IOException e){System.err.println("Couldn't get response");}
+    }
+    
     public static void main(String[] args) {
 String hostName = "192.168.0.100";
 int portNumber = 10000;
 
 Socket robotSocket = null;
-DataOutputStream out = null;
-DataInputStream in = null;
-    byte[] response = new byte[100];
+    
 try {
      robotSocket = new Socket();
      SocketAddress sockaddr = new InetSocketAddress(hostName, portNumber);
@@ -21,32 +42,9 @@ try {
 catch(IOException e) {
 	System.err.println("Couldn't get I/O for robot");
     }
-
- try{
-	System.out.println("Starting to read");
-
-	in.read(response);
-	String responseString = new String(response,"UTF-8");
-	System.out.println(responseString);
-	
-    }catch(IOException e){System.err.println("Couldn't get response");}
-System.out.println("Activating robot");
-try{
-    out.writeBytes("ActivateRobot<\0>");
-}catch(IOException e){System.err.println("Couldn't write to robot");}
-
-    System.out.println("ActivateRobot SENT");
-
-    int c = -1;
-
-    try{
-	System.out.println("Starting to read");
-
-	in.read(response);
-	System.out.println(Arrays.toString(response));
-	
-    }catch(IOException e){System.err.println("Couldn't get response");}
-    System.out.println(response);
+ReadMessage();
+SendMessage("ActivateRobot");
+ReadMessage();
     try{
     robotSocket.close();
     }catch(IOException e){System.err.println("Couldn't close robot");}
