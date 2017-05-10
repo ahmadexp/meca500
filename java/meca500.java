e962c95ba394a7f848afc4211a137543a91f0587
Clancy Emanuel <clancyemanuel@gmail.com>
2017-05-09 22:53:30 -0400

    Need to read before sending anything

--- java/meca500.java
+++ java/meca500.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 
 class meca500{
     public static void main(String[] args) {
@@ -7,24 +8,48 @@ String hostName = "192.168.0.100";
 int portNumber = 10000;
 
 Socket robotSocket = null;
-PrintWriter out = null;
-BufferedReader in = null;
-
+DataOutputStream out = null;
+DataInputStream in = null;
+    byte[] response = new byte[100];
 try {
      robotSocket = new Socket();
      SocketAddress sockaddr = new InetSocketAddress(hostName, portNumber);
      robotSocket.connect(sockaddr, 3000);
-     out = new PrintWriter(robotSocket.getOutputStream(), true);
-     in = new BufferedReader(new InputStreamReader(robotSocket.getInputStream()));
+     out = new DataOutputStream(robotSocket.getOutputStream());
+     in = new DataInputStream(robotSocket.getInputStream());
 }catch(UnknownHostException e){System.err.println("Couldn't find host");}
 catch(IOException e) {
 	System.err.println("Couldn't get I/O for robot");
     }
-    out.println("ActivateRobot");
+
+ try{
+	System.out.println("Starting to read");
+
+	in.read(response);
+	System.out.println(Arrays.toString(response));
+	
+    }catch(IOException e){System.err.println("Couldn't get response");}
+System.out.println("Activating robot");
+try{
+    out.writeBytes("ActivateRobot<\0>");
+}catch(IOException e){System.err.println("Couldn't write to robot");}
+
+    System.out.println("ActivateRobot SENT");
+
+    int c = -1;
+
     try{
-System.out.println(in.readLine());
-    }catch(IOException e){System.err.println("Couldn't read line from robot");}
-        }
+	System.out.println("Starting to read");
+
+	in.read(response);
+	System.out.println(Arrays.toString(response));
+	
+    }catch(IOException e){System.err.println("Couldn't get response");}
+    System.out.println(response);
+    try{
+    robotSocket.close();
+    }catch(IOException e){System.err.println("Couldn't close robot");}
+    }
 }
 
     
